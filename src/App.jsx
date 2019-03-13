01ff6bb486b82c23a1269f2a01b208df6f7a148f
jun <jun@finent.io>
2019-03-13 11:57:46 +0900

    Another feature

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>Feature!</p>
+          <p>Another Feature!</p>
         </header>
       </div>
     );
